ea1174b25282863529cc697ec872cd4728921180
HoangT <ac5217@student.jamk.fi>
2022-08-17 11:17:25 +0300

    adding dice game project

---
+++ index.js
@@ -0,0 +1,17 @@
+// Create variables fo the game state
+let player1Score = 0;
+let player2Score = 0;
+let player1Turn = true;
+//player 1
+const player1 = document.getElementById("player1");
+const player1Scoreboard = document.getElementById("player1Scoreboard");
+const player1Dice = document.getElementById("player1Dice");
+
+//player 2
+const player2 = document.getElementById("player2");
+const player2Scoreboard = document.getElementById("player2Scoreboard");
+const player2Dice = document.getElementById("player2Dice");
+
+//Button
+const rollBtn = document.getElementById("rollBtn");
+const resetBtn = document.getElementById("resetBtn");
